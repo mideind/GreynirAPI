6818a5b48e0f522d680a00541811d28d0ae08bb6
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-09-30 17:14:08 +0000

    Added tests for noun phrase API

--- tests/test_app.py
+++ tests/test_app.py
@@ -21,12 +21,73 @@
 
 """
 
+from urllib.parse import urlencode
+from fastapi.testclient import TestClient
+from main import app
 
-import pytest
-from aiohttp import web
+
+client = TestClient(app)
 
 
-# @pytest.fixture
-# def cli(loop, aiohttp_client):
-#     app = web.Application()
-#     return loop.run_until_complete(aiohttp_client(app))
+def test_np_api():
+    """ Test noun phrase API ("/np"). """
+
+    # Test without params
+    response = client.get("/np")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["err"] is True
+    assert json["errmsg"]
+
+    # Test w. just noun phrase param
+    n = "Maðurinn með hattinn"
+
+    response = client.get(f"/np?{urlencode(dict(q=n))}")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["err"] is False
+    assert "cases" in json
+    assert json["cases"]["nf"] == n
+    assert json["cases"]["þf"] == "Manninn með hattinn"
+    assert json["cases"]["þgf"] == "Manninum með hattinn"
+    assert json["cases"]["ef"] == "Mannsins með hattinn"
+
+    # Test w. case param
+    response = client.get(f"/np?{urlencode(dict(q=n, case='ef'))}")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["err"] is False
+    assert "cases" in json
+    assert "nf" not in json["cases"]
+    assert "þf" not in json["cases"]
+    assert "þgf" not in json["cases"]
+    assert json["cases"]["ef"] == "Mannsins með hattinn"
+
+    # Test w. number param and ambiguous word ("fata" can be sing. nom. or pl. gen. of "föt")
+    response = client.get(f"/np?{urlencode(dict(q='fata', number='et'))}")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["err"] is False
+    assert "cases" in json
+    assert json["cases"]["nf"] == "fata"
+    assert json["cases"]["þf"] == "fötu"
+    assert json["cases"]["þgf"] == "fötu"
+    assert json["cases"]["ef"] == "fötu"
+
+    response = client.get(f"/np?{urlencode(dict(q='fata', number='ft'))}")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["err"] is False
+    assert "cases" in json
+    assert json["cases"]["nf"] == "föt"
+    assert json["cases"]["þf"] == "föt"
+    assert json["cases"]["þgf"] == "fötum"
+    assert json["cases"]["ef"] == "fata"
+
+    # Test w. both case and number params
+    response = client.get(f"/np?{urlencode(dict(q='fata', case='þgf', number='ft'))}")
+    assert response.status_code == 200
+    json = response.json()
+    assert json["err"] is False
+    assert "cases" in json
+    assert json["cases"]["þgf"] == "fötum"
